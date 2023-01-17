3074e1abbbe665e6f037bc34957cd360f8e67993
Mihai Gritcan <mihail.gritcan@arobs.com>
2023-01-17 22:59:11 +0200

    remove unnecessary references

--- src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export { SpeechModule } from './module';
-export { SpeechService } from './speech.service';
-export { SpeechActionDirective } from './speech-action.directive';
-export { SpeechContextDirective } from './speech-context.directive';
