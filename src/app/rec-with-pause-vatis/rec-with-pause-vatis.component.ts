af153d8d15ba821bb7f4b7c59654207fff0e4d34
Mihai Gritcan <mihail.gritcan@arobs.com>
2023-05-08 12:52:42 +0300

    allow user to start manually the recording

--- src/app/rec-with-pause-vatis/rec-with-pause-vatis.component.ts
+++ src/app/rec-with-pause-vatis/rec-with-pause-vatis.component.ts
@@ -140,7 +140,7 @@ export class RecWithPauseVatisComponent implements OnInit, AfterViewInit, OnDest
     }
   }
 
-  private onStartRec() {
+  public onStartRec() {
     // this.setFocusInFirstInput();
     this.toggleRec();
   }
